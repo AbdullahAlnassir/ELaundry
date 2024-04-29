811e292fbed488156f24614f7926883fa960f6a8
AbdullahAlnassir <145174367+AbdullahAlnassir@users.noreply.github.com>
2024-04-28 22:43:32 -0400

    few mods

--- public/home.js
+++ public/home.js
@@ -1,4 +1,3 @@
 document.getElementById('sign-up').addEventListener('click', function() {
-    // Code to open sign-up modal or redirect to sign-up page
-    alert('Sign up functionality not yet implemented.');
+    window.location.href = 'register.html';
 });
